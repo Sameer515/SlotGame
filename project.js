87314585ec9db4233aad52cc7cbc29f8cc8c1fed
Sameer Khassawneh <Sameer515@hotmail.com>
2023-11-18 22:18:11 +0200

    update v 0.0.2

--- project.js
+++ project.js
@@ -8,6 +8,23 @@
 
 const prompt = require('prompt-sync')();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+	A: 2,
+	B: 4,
+	C: 6,
+	D: 8,
+};
+
+const SYMBOL_VALUES = {
+	A: 5,
+	B: 4,
+	C: 3,
+	D: 2,
+};
+
 const deposit = () => {
 	while (true) {
 		const depositAmount = prompt('Enter a Deposit Amount:');
@@ -41,23 +58,61 @@ const getNumberofbetlines = () => {
 	}
 };
 
-const getBet= (balance)=>{
-    while (true) {
-		const bet = prompt(
-			'Enter the total bet:'
-		);
+const getBet = (balance, lines) => {
+	while (true) {
+		const bet = prompt('Enter bet per line:');
 		const numberBet = parseFloat(bet);
 		if (
 			isNaN(numberBet) ||
 			numberBet <= 0 ||
-			numberBet > balance
+			numberBet > balance / lines
 		) {
-			console.log('Invalid number of lines');
+			console.log('Invalid bet, try again');
 		} else {
-			return numberofLines;
+			return numberBet;
 		}
 	}
-}
+};
+
+const spin = () => {
+	const symbols = [];
+	for (const [symbol, count] of Object.entries(
+		SYMBOLS_COUNT
+	)) {
+		for (let i = 0; i < count; i++) symbols.push(symbol);
+	}
+	const reels = [];
+
+	for (let i = 0; i < COLS; i++) {
+		reels.push([]);
+		const reelSymbols = [...symbols];
+		for (let j = 0; j < ROWS; j++) {
+			const randomIndex = Math.floor(
+				Math.random() * reelSymbols.length
+			);
+			const selectedSymbol = reelSymbols[randomIndex];
+			reels[i].push(selectedSymbol);
+			reelSymbols.splice(randomIndex, 1);
+		}
+	}
+	return reels;
+	//console.log(symbols);
+};
+
+const transpose = (reels) => {
+	const rows = [];
+	for (let i = 0; i < ROWS; i++) {
+		rows.push([]);
+		for (j = 0; j < COLS; j++) {
+			row[i].push(reels[j][i]);
+		}
+	}
+	return rows;
+};
 
 let balance = deposit();
 const numberofLines = getNumberofbetlines();
+const bet = getBet(balance, numberofLines);
+const reels = spin();
+const rows =transpose(reels);
+console.log(transpose);
